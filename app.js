60bcb48c0f5501d9cbd7b98cefc68e1cdc9c2b5a
Yuvraj Singh <yuvraj0157@gmail.com>
2024-06-08 19:59:03 +0530

    chore: Update error handling in app.js

--- app.js
+++ app.js
@@ -57,3 +57,9 @@ app.get("/", isAuth, (req, res) => {
 app.use("*",(req,res)=>{
   res.status(404).render("404");
 });
+
+app.use((err,req,res,next)=>{
+  console.error(`Error occurred in route: ${req.path}`);
+  console.error(err.stack);
+  res.status(500).render("500");
+});
